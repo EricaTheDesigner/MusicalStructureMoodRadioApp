2254f033e55ea736c2b37d8a54bce94208602638
EricaTheDesigner <Erica@EricaTheDesigner.com>
2018-07-31 22:45:00 -0400

    app now runs

--- app/src/main/java/com/example/android/musicalstructuremoodradioapp/MusicAdapter.java
+++ app/src/main/java/com/example/android/musicalstructuremoodradioapp/MusicAdapter.java
@@ -7,8 +7,6 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 
 public class MusicAdapter extends ArrayAdapter<Music> {
@@ -17,9 +15,10 @@ public class MusicAdapter extends ArrayAdapter<Music> {
     }
 
     @Override
-    public view getView(int position, View convertView, ViewGroup parent){
+    public View getView(int position, View convertView, ViewGroup parent){
 
         //Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
 
         if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
@@ -29,7 +28,7 @@ public class MusicAdapter extends ArrayAdapter<Music> {
         TextView songNameTextView = (TextView) listItemView.findViewById(R.id.song_name_text_view);
         songNameTextView.setText(my_music.getmSongName());
         TextView artistNameTextView = (TextView) listItemView.findViewById(R.id.artist_name_text_view);
-        songNameTextView.setText(my_music.getmArtistName());
+        songNameTextView.setText(my_music.getmSongArtist());
 
         return listItemView;
     }
